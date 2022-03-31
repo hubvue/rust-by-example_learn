7eae44140c7e5b00d959e0ee6a98a546f3fea03d
hubvue <cckcc.kim@gmail.com>
2022-03-31 16:57:56 +0800

    feat: match

--- lession8/match.rs
+++ lession8/match.rs
@@ -66,4 +66,45 @@ fn main() {
     Color::CMYK(c, m, y, k) => println!("c: {}, m: {}, y: {}, k: {}", c, m, y, k)
   }
 
+  // 解构指针和引用
+  println!("-------------解构指针和引用---------------");
+  // 对指针来说，解构和解引用要区分，因为这二者的概念是不同的。
+  // 解引用使用*
+  // 解构使用 &、ref和ref mut
+
+  let reference = &4; // 获得一个i32类型的引用
+  match reference {
+    // 如果用 &val 这个模式去匹配 reference，就相当于这样的比较：
+    // &i32 即 reference 的类型
+    // &val 即 用于匹配的模式
+    // 进行匹配，如果去掉匹配的&， 那么 val 就是i32，
+    &val => println!("Got a value via : {:?}", val)
+  }
+  // 如果不想用 & 来结解构引用， 需要在匹配前来解引用
+  match *reference {
+    val => println!("Got a value via: {:?}", val)
+  }
+
+  // 如果一开始就不是引用
+  let _not_a_reference = 3;
+  // Rust 对这种情况提供了 ref。它更改了赋值行为，从而可以对具体值创建引用
+  // 下面代码得到一个引用
+  let ref _is_a_reference = 3;
+  match _is_a_reference {
+    ref r => println!("value: {}", r)
+  }
+
+  // 相应的，定义两个非引用的变量，通过 ref 和 ref mut应该可以其到其引用
+  let value = 5;
+  let mut mut_value = 6;
+
+  match value {
+    ref r => println!("value: {}", r)
+  }
+  match mut_value {
+    ref mut m => {
+      *m += 10;
+      println!("value: {:?}", m);
+    }
+  }
 }
