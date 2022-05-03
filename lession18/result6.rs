a4f3d6ce9bbe8934825583b5828ea56d2ea44859
hubvue <cckcc.kim@gmail.com>
2022-05-03 16:14:59 +0800

    feat: try

---
+++ lession18/result6.rs
@@ -0,0 +1,24 @@
+// try! 宏
+
+// 在 ？出现之前，相同的功能是使用try!宏完成的。现在我们推荐使用 ? 运算符。
+
+use std::num::ParseIntError;
+
+fn multiply(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+    let first_number = try!(first_number_str.parse::<i32>());
+    let second_number = try!(second_number_str.parse::<i32>());
+
+    Ok(first_number * second_number)
+}
+
+fn print(result: Result<i32, ParseIntError>) {
+    match result {
+        Ok(n) => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn main() {
+    print(multiply("10", "2"));
+    print(multiply("t", "2"));
+}
